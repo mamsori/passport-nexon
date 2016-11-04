de668afb860453fd6acc2156b80b009e8f09ba16
HyoukJoon Lee <hjlee@nexon.net>
2016-11-03 18:35:43 -0700

    migrated from local repository

---
+++ test/index-test.js
@@ -0,0 +1,22 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var google = require('passport-nexon');
+
+vows.describe('passport-nexon').addBatch({
+  
+  'module': {
+    'should report a version': function (x) {
+      assert.isString(google.version);
+    },
+    'should export OAuth strategy': function (x) {
+      assert.isFunction(google.Strategy);
+      assert.isFunction(google.OAuthStrategy);
+      assert.equal(google.Strategy, google.OAuthStrategy);
+    },
+    'should export OAuth 2.0 strategy': function (x) {
+      assert.isFunction(google.OAuth2Strategy);
+    },
+  },
+  
+}).export(module);
---
+++ test/oauth2-test.js
@@ -0,0 +1,124 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var NexonStrategy = require('passport-nexon/oauth2');
+
+vows.describe('NexonStrategy').addBatch({
+  
+  'strategy': {
+    topic: function() {
+      return new NexonStrategy({
+        productID: 'ABC123',
+        clientSecret: 'secret',
+        authorizationURL: 'url',
+        tokenURL: 'url'
+      },
+      function() {});
+    },
+    
+    'should be named google': function (strategy) {
+      assert.equal(strategy.name, 'nexon');
+    },
+  },
+  
+  'strategy when loading user profile': {
+    topic: function() {
+      var strategy = new GoogleStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+      
+      // mock
+      strategy._oauth2.get = function(url, accessToken, callback) {
+        var body = '{ \
+         "id": "00000000000000", \
+         "email": "fred.example@gmail.com", \
+         "verified_email": true, \
+         "name": "Fred Example", \
+         "given_name": "Fred", \
+         "family_name": "Example", \
+         "picture": "https://lh5.googleusercontent.com/-2Sv-4bBMLLA/AAAAAAAAAAI/AAAAAAAAABo/bEG4kI2mG0I/photo.jpg", \
+         "gender": "male", \
+         "locale": "en-US" \
+        }';
+        
+        callback(null, body, undefined);
+      }
+      
+      return strategy;
+    },
+    
+    'when told to load user profile': {
+      topic: function(strategy) {
+        var self = this;
+        function done(err, profile) {
+          self.callback(err, profile);
+        }
+        
+        process.nextTick(function () {
+          strategy.userProfile('access-token', done);
+        });
+      },
+      
+      'should not error' : function(err, req) {
+        assert.isNull(err);
+      },
+      'should load profile' : function(err, profile) {
+        assert.equal(profile.provider, 'google');
+        assert.equal(profile.id, '00000000000000');
+        assert.equal(profile.displayName, 'Fred Example');
+        assert.equal(profile.name.familyName, 'Example');
+        assert.equal(profile.name.givenName, 'Fred');
+        assert.equal(profile.emails[0].value, 'fred.example@gmail.com');
+      },
+      'should set raw property' : function(err, profile) {
+        assert.isString(profile._raw);
+      },
+      'should set json property' : function(err, profile) {
+        assert.isObject(profile._json);
+      },
+    },
+  },
+  
+  'strategy when loading user profile and encountering an error': {
+    topic: function() {
+      var strategy = new GoogleStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+      
+      // mock
+      strategy._oauth2.get = function(url, accessToken, callback) {
+        callback(new Error('something-went-wrong'));
+      }
+      
+      return strategy;
+    },
+    
+    'when told to load user profile': {
+      topic: function(strategy) {
+        var self = this;
+        function done(err, profile) {
+          self.callback(err, profile);
+        }
+        
+        process.nextTick(function () {
+          strategy.userProfile('access-token', done);
+        });
+      },
+      
+      'should error' : function(err, req) {
+        assert.isNotNull(err);
+      },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
+      'should not load profile' : function(err, profile) {
+        assert.isUndefined(profile);
+      },
+    },
+  },
+  
+}).export(module);
